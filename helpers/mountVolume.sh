1840751cc4834cb86467de556dbaa13b0650b0c9
LucaFeger <luca.feger@outlook.de>
2018-10-22 00:06:48 +0200

    initial commit

---
+++ helpers/mountVolume.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+command -v curl >/dev/null || { echo "Installing curl..."; sudo apt install -y curl; }
+command -v jq >/dev/null || { echo "Installting jq..."; sudo aput install -y jq; }
+
+# Get Hetzner Server ID
+IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+IP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -H "Authorization: Bearer $1" https://api.hetzner.cloud/v1/servers)
+
+SERVER_OBJECT=$(jq '.servers[]|select(.public_net.ipv4.ip=="'"$IP"$'")' <<< "$IP_RESPONSE")
+SERVER_ID=$(jq ".id" <<< "$SERVER_OBJECT")
+
+# POST Attach Volume to Hetzner
+RESULT=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $1" -d '{"server": '$SERVER_ID$'}' https://api.hetzner.cloud/v1/volumes/$2/actions/attach)
+HTTP_BODY=$(echo "$RESULT" | sed -e 's/HTTPSTATUS\:.*//g')
+ERROR_CHECK=$(jq '.error.message' <<< "$HTTP_BODY")
+
+if [ "$ERROR_CHECK" = "null" ]; then
+	echo "Mounting volume..."
+	LINUX_TEMP="$3"
+	MOUNT_TEMP="$4"
+	
+	LINUX_DEVICE="${LINUX_TEMP//\"}"
+	MOUNT_PATH="/mnt/$MOUNT_TEMP"
+	
+	echo "Waiting for the container to create (7 Seconds)"
+	sleep 7
+
+        echo "Remove existing mounts"
+        sudo umount -R "$MOUNT_PATH" &> /dev/null
+        echo "removing old directory"
+        sudo rm -rf "$MOUNT_PATH" &> /dev/null
+
+        echo "mounting to directory"
+
+        sudo mkfs.ext4 -F "$LINUX_DEVICE" &>/dev/null
+        sudo mkdir "$MOUNT_PATH" &>/dev/null
+        mount -o discard,defaults "$LINUX_DEVICE" "$MOUNT_PATH" &> /dev/null
+        echo "The volume is now mounted to $MOUNT_PATH !"	
+else
+	echo "An error occured: $ERROR_CHECK"
+fi
---
+++ volumes.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+DIALOG=${DIALOG=dialog}
+
+command -v dialog >/dev/null 2>&1 || { echo "installing dialog..."; sudo apt install -y dialog; }
+command -v curl >/dev/null 2>&1 || { echo "installing curl..."; sudo apt install -y curl; }
+command -v jq >/dev/null 2>&1 ||{ echo "installing jq..."; sudo apt install -y jq; }
+
+ANSWER=$(dialog --title "Choose action" --default-item "1" \
+       	--menu "Select:" 0 0 0 \
+	1 "Create and mount volume" \
+	2 "Mount volume" \
+	3 "Unmount volume" \
+	4 "Delete and unmount volume" 3>&1 1>&2 2>&3)
+clear
+case $ANSWER in
+	1)
+		# BEGINNING OF SECTION "CREATE AND MOUNT VOLUME"
+
+		SIZE=$(dialog --title "Volume Setup" \
+			--inputbox "Enter the volume size in GB (min 10):" 8 40 \
+  			3>&1 1>&2 2>&3 3>&-)
+		dialog --clear
+		NAME=$(dialog --title "Volume Setup" --inputbox "Enter the volume name:" 8 40 3>&1 1>&2 2>&3 3>&-)
+		clear
+
+		./helpers/createVolume.sh "$1" "$SIZE" "$NAME";;
+	2)
+		# BEGINNING OF SECTION "MOUNT VOLUME"
+
+		ALL_VOLUMES_HTTP=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -H "Authorization: Bearer $1" https://api.hetzner.cloud/v1/volumes)
+		ALL_VOLUME_NAMES=$(echo $ALL_VOLUMES_HTTP | jq -r '.volumes[].name')
+
+			
+		VALUES=""
+		for i in $ALL_VOLUME_NAMES; do
+			if [ "$(jq -r '.volumes[]|select(.name=="'$i'")|.server' <<< "$ALL_VOLUMES_HTTP")" == "null" ]; then
+				VALUES="$VALUES $(jq -r '.volumes[]|select(.name=="'$i'")|.id' <<< "$ALL_VOLUMES_HTTP") $i"
+			fi
+		done
+		
+		if [-z $VALUES]; then
+			echo "There is no volume that isn't mounted. Aborting..."
+			exit 1	
+		else
+			SELECTED_VOLUME_ID=$(dialog --title "Volume mount" --menu "Select: " 0 0 0 $VALUES 3>&1 1>&2 2>&3)
+			clear
+		fi
+
+		./helpers/mountVolume.sh "$1" "$SELECTED_VOLUME_ID" "$(jq -r '.volumes[]|select(.id=='$SELECTED_VOLUME_ID')|.linux_device' <<< "$ALL_VOLUMES_HTTP")"  "$(jq -r '.volumes[]|select(.id=='$SELECTED_VOLUME_ID')|.name' <<< "$ALL_VOLUMES_HTTP")"
+		;;
+	[3-4])
+		# BEGINNING OF SECTION UNMOUNT VOLUME
+		ALL_VOLUMES_HTTP=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -H "Authorization: Bearer $1" https://api.hetzner.cloud/v1/volumes)
+		ALL_VOLUME_NAMES=$(echo $ALL_VOLUMES_HTTP | jq -r '.volumes[].name')
+
+		ALL_SERVERS_HTTP=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -H "Authorization: Bearer $1" https://api.hetzner.cloud/v1/servers)
+
+		IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
+
+                VALUES=""
+                for i in $ALL_VOLUME_NAMES; do
+			server_id=$(jq -r '.volumes[]|select(.name=="'$i'")|.server' <<< "$ALL_VOLUMES_HTTP")
+			server_ip=$(jq -r '.servers[]|select(.id=='$server_id')|.public_net.ipv4.ip' <<< "$ALL_SERVERS_HTTP")
+
+			echo "$IP"
+			echo "$server_ip"
+
+                        if [ "$server_id" != "null" ] && [ "$server_ip" == "$IP" ]; then
+                                VALUES="$VALUES $(jq -r '.volumes[]|select(.name=="'$i'")|.id' <<< "$ALL_VOLUMES_HTTP") $i"
+                        fi
+                done
+		
+		if [ -z $VALUES ]; then
+			echo "There are no mounted volumes. Aborting..."
+			exit 1
+		else
+			SELECTED_VOLUME_ID=$(dialog --title "Unmount volume" --menu "Select: " 0 0 0 $VALUES 3>&1 1>&2 2>&3)
+			clear
+		fi
+		
+		if [ $ANSWER == 4 ]; then
+			./helpers/unmountVolume.sh "$1" "$SELECTED_VOLUME_ID"  "$(jq -r '.volumes[]|select(.id=='$SELECTED_VOLUME_ID')|.name' <<< "$ALL_VOLUMES_HTTP")" true
+		else
+			./helpers/unmountVolume.sh "$1" "$SELECTED_VOLUME_ID"  "$(jq -r '.volumes[]|select(.id=='$SELECTED_VOLUME_ID')|.name' <<< "$ALL_VOLUMES_HTTP")" false
+		fi
+		;;	
+esac	
